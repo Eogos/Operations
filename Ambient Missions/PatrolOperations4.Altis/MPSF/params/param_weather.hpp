1a9c1040571ed0a4769e2855ad547e33947bfe95
Unknown <briansherbino@hotmail.com>
2018-01-22 22:42:56 -0500

    PO4/3
    Added Zeus Slots
    Limit of Active Operations = Infinite
    Base Selection = Charlie, Freedom (Hopefully?)
    Mission Duration in Operation Completions = Infinite
    Revive MP = Disabled
    Time = 4x
    Zeus = Unrestricted

--- Ambient Missions/PatrolOperations4.Altis/MPSF/params/param_weather.hpp
+++ Ambient Missions/PatrolOperations4.Altis/MPSF/params/param_weather.hpp
@@ -17,7 +17,7 @@ class ParamWeather {
 class ParamTimeSpeed {
 	title = "Mission Time Acceleration (Game : Reality)";
 	values[] = {0,1,2,3,4,5,6,7};
-	default = 2;
+	default = 6;
 	texts[] = {"x0.1 (1:10Hrs)","0.5 (1:2Hrs)","x1 (1:1Hrs)","x2 (2:1Hrs)","x4 (4:1Hrs)","x6 (6:1Hrs)","x12 (12:1Hrs)","x24 (24:1Hrs)"};
 //	function = "MPSF_Param_fnc_setMissionSpeed";
 };
